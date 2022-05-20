f582b979481827f014acda7b36f7a4bf15002847
AdamKh <adamkhashiev2004@mail.ru>
2022-05-20 12:28:50 +0300

    update usl

--- usl.py
+++ usl.py
@@ -6,10 +6,8 @@ if __name__ == '__main__':
     s = input('Введите предложение:\n')
     s1 = s.split()
     i = 0
-    for i in range(len(s1)):
-        for j in range(len(s1)-1):
-            if len(s1[j]) < len(s1[j + 1]):
-                s1[j], s1[j + 1] = s1[j + 1], s1[j]
+    s1.sort(key=len)
+    s1.reverse()
     s = ''
     for i in range(0, len(s1)):
         s += s1[i] + ' '
