1c67a35fea72cced1290fa4d41e485060a33031c
AdamKh <adamkhashiev2004@mail.ru>
2022-05-20 12:35:46 +0300

    reverse

--- usl.py
+++ usl.py
@@ -6,8 +6,7 @@ if __name__ == '__main__':
     s = input('Введите предложение:\n')
     s1 = s.split()
     i = 0
-    s1.sort(key=len)
-    s1.reverse()
+    s1.sort(key=len, reverse=True)
     s = ''
     for i in range(0, len(s1)):
         s += s1[i] + ' '
